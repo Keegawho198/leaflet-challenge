021c9e2fcffb6f832b149ff122b4c6dd88bd1f43
Keegawho198 <keegan.nair@hotmail.com>
2024-11-28 18:35:53 +1100

    Adding code for part 1

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # leaflet-challenge
+
+
+
+The code creates a Leaflet map centered at coordinates (0, 0) with a zoom level of 2.
+
+
+It fetches earthquake data from the USGS GeoJSON feed.
+
+
+For each earthquake, it creates a circle marker whose size is based on the magnitude and color is based on the depth.
+
+
+Popups provide additional information about each earthquake when the markers are clicked.
---
+++ static/js/logic.js
@@ -0,0 +1,65 @@
+//JSON URL
+
+// https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_hour.geojson
+
+
+
+
+// Create the map
+var map = L.map('map').setView([0, 0], 2);
+
+// Add a tile layer
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+
+// Fetch earthquake data
+d3.json('https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_day.geojson').then(function(data) {
+    // Function to determine marker size based on magnitude
+    function markerSize(magnitude) {
+        return magnitude * 4; // Adjust the multiplier as needed
+    }
+
+    // Function to determine marker color based on depth
+    function getColor(depth) {
+        return depth > 100 ? '#FF0000' :
+               depth > 50  ? '#FF7F00' :
+               depth > 20  ? '#FFFF00' :
+               depth > 0   ? '#7FFF00' :
+                             '#00FF00';
+    }
+
+    // Loop through the features in the GeoJSON data
+    data.features.forEach(function(feature) {
+        var coords = feature.geometry.coordinates;
+        var magnitude = feature.properties.mag;
+        var depth = coords[2];
+        var color = getColor(depth);
+        var size = markerSize(magnitude);
+
+        // Create a circle marker for each earthquake
+        L.circleMarker([coords[1], coords[0]], {
+            radius: size,
+            fillColor: color,
+            color: '#000',
+            weight: 1,
+            opacity: 1,
+            fillOpacity: 0.8
+        }).addTo(map)
+        .bindPopup("Magnitude: " + magnitude + "<br>Depth: " + depth + " km<br>" + feature.properties.place);
+    });
+
+    // Create a legend
+    var legend = L.control({position: 'bottomright'});
+    legend.onAdd = function () {
+        var div = L.DomUtil.create('div', 'legend');
+        div.innerHTML += '<strong>Depth (km)</strong><br>';
+        div.innerHTML += '<i style="background: #FF0000"></i> > 100<br>';
+        div.innerHTML += '<i style="background: #FF7F00"></i> 50 - 100<br>';
+        div.innerHTML += '<i style="background: #FFFF00"></i> 20 - 50<br>';
+        div.innerHTML += '<i style="background: #7FFF00"></i> 0 - 20<br>';
+        div.innerHTML += '<i style="background: #00FF00"></i> < 0<br>';
+        return div;
+    };
+    legend.addTo(map);
+});
